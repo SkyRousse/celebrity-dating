6c4115caabeffc7306118fee5b17566d7ef5fd4f
Kevin VanEvery and Sky Rousse <kevin@>
2016-06-02 10:06:44 -0700

    create javascript to show hide dolly or justin based on form input

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
 /*This file is for your custom js.  All yours*/
+$(function () {
+  $("form#celebrity-survey").submit(function(event) {
+    event.preventDefault();
+    debugger;
+    var age = parseInt($("input#age").val());
+    var gender = $("input#gender").val();
+    var music = $("input[name=music]:checked").val();
+    var food = $("input[name=food]:checked").val();
+
+    if (age >= 50 && gender === "male" && music === "country") {
+      $("#dolly-parton").show();
+    }
+
+    else if (age <= 20 && gender === "female" && music === "pop") {
+      $("#justin-bieber").show();
+    }
+
+    else {
+      $("#no-celebrities").show();
+    }
+  });
+});
